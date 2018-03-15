8ef5f1b1a4982b7ec27226dcd61277abf5ece4e8
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2018-03-15 14:02:43 +0500

    Order by rid and plot side-by-side histograms

--- analysis.R
+++ analysis.R
@@ -1,6 +1,9 @@
 s <- read.csv('./data/sp18-bcs-A.csv', stringsAsFactors=F)
 a <- read.csv('./data/answers.csv', stringsAsFactors=F)$answer			# Read the answers in to a list
 
+# Sort s on the basis of 'rid'
+s <- s[order(s$rid),]
+
 # From s create a data-frame r which details whether the response to each question was right or wrong (T or F)
 r <- s[,c("rid", "qid")]
 
@@ -11,3 +14,15 @@ for (i in 1:30) {
 	r[[key]] = s[[key]] == a[i]			# Compare the value in s$Xi to the actual answer (a[i]) and store it in 'r'
 										# Note the use of [[]] instead of $ (which is a short-hand for [[]]) to access the column of the data-frame when using a variable to hold the column name
 }
+
+# Calculate the total for each student by summing over the Xi entries (which are columns 3 to 32)
+r$tot <- rowSums(r[,3:32])		
+
+
+# Graph data
+library(ggplot2)
+
+# Plot side-by-side histograms of the total scores earned for each qid (type / version mix)
+p <- ggplot(r, aes(tot, fill=qid)) + geom_histogram(alpha = 0.5, breaks=seq(0,30,by=2), aes(y = ..count..), position ='dodge')
+
+p2 <- ggplot(r[r$qid == "v2" | r$qid == "v3",], aes(tot, fill=qid)) + geom_histogram(alpha = 0.5, breaks=seq(0,30,by=2), aes(y = ..count..), position ='dodge')
