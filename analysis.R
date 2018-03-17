b5e59ffabbbfb0cfeab63351a7fc1bbab5ad9546
Abid H Mujtaba <abid.naqvi83@gmail.com>
2018-03-18 00:05:39 +0500

    Bar plot of responses to Q.1

--- analysis.R
+++ analysis.R
@@ -28,17 +28,19 @@ library(ggplot2)
 # p <- ggplot(r, aes(tot, fill=qid)) + geom_histogram(alpha = 0.5, breaks=seq(0,30,by=2), aes(y = ..count..), position ='dodge')
 # p2 <- ggplot(r[r$qid == "v2" | r$qid == "v3",], aes(tot, fill=qid)) + geom_histogram(alpha = 0.5, breaks=seq(0,30,by=2), aes(y = ..count..), position ='dodge')
 
-# Plot results for each question.
-
-# On an single plot show the number of correct responses to each question.
-
 # Create a data-frame containing the question number and the number of correct response to each question
 # For the latter we use 'colSums' to calculate the number of correct results.
-df = data.frame( question = seq(1,30), correct = colSums( r[,3:32] ) )
+# df = data.frame( question = seq(1,30), correct = colSums( r[,3:32] ) )
 # p3 = ggplot(df, aes(x = question, y = correct)) + geom_col() + ggtitle('Number of correct responses to each question') + xlab('Question #') + ylab('Number Correct')
-p3 = ggplot(df, aes(x = question, y = correct)) + geom_col() + 
-												ggtitle('Number of correct responses to each question') + 
-												scale_x_discrete("Question #", limits=seq(1,30)) +
-												scale_y_discrete("Number Correct", limits=seq(0,25,5))
+# p3 = ggplot(df, aes(x = question, y = correct)) + geom_col() + 
+# 												ggtitle('Number of correct responses to each question') + 
+# 												scale_x_discrete("Question #", limits=seq(1,30)) +
+# 												scale_y_discrete("Number Correct", limits=seq(0,25,5))
 
 # Plot the results for Q.1 (bar-plot of answers and indication of the correct result).
+pq1 = ggplot( count( s[,c("q1")] ), aes(x = x, y = freq, fill = x)) + geom_col() +				# Each value of x can have a different fill color
+															ggtitle('Responses to Q.1') +
+															xlab("Response") +
+															ylab("Frequency") + 
+															scale_fill_manual(values = c("grey50", "grey50", "green", "grey50")) +		# Manually specify the fill colors
+															theme(legend.position = "none")		# Remove legend introduced by manual fill
