4b84961f7ef3ab52cd296326d6ca983b2d4e7c8e
Abid H Mujtaba <abid.naqvi83@gmail.com>
2018-03-17 17:20:56 +0500

    correct ids after sorting. Modify to use new column names preceded by 'q' instead of the automatically inserted 'X'

--- analysis.R
+++ analysis.R
@@ -2,6 +2,7 @@ s <- read.csv('./data/sp18-bcs-A.csv', stringsAsFactors=F)
 a <- read.csv('./data/answers.csv', stringsAsFactors=F)$answer			# Read the answers in to a list
 
 # Sort s on the basis of 'rid'
+s <- s[,-1]					# Remove id column because we are about to sort
 s <- s[order(s$rid),]
 rownames(s) <- NULL			# Update the row ids to unscramble them
 
@@ -11,8 +12,8 @@ r <- s[,c("rid", "qid")]
 # Loop over the 30 answers
 for (i in 1:30) {
 
-	key <- paste("X", i, sep="")		# Create the X prefixed column name for the i-th question
-	r[[key]] = s[[key]] == a[i]			# Compare the value in s$Xi to the actual answer (a[i]) and store it in 'r'
+	key <- paste("q", i, sep="")		# Create the 'q' prefixed column name for the i-th question
+	r[[key]] = s[[key]] == a[i]			# Compare the value in s$qi to the actual answer (a[i]) and store it in 'r'
 										# Note the use of [[]] instead of $ (which is a short-hand for [[]]) to access the column of the data-frame when using a variable to hold the column name
 }
 
